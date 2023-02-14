bc1573923166c19072fc8c10bda5beec3f7176f2
zhandos <tursynbaevzhandos@gmail.com>
2023-02-14 18:58:02 +0600

    add app crm

--- website/urls.py
+++ website/urls.py
@@ -1,4 +1,4 @@
-"""myproject URL Configuration
+"""website URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from crm.views import first_page, thanks_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', first_page, name='index'),
+    path('thanks/', thanks_page, name='thanks'),
 ]
