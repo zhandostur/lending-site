66348b68af5a32fceac785680bbbff6e3bf80131
zhandos <tursynbaevzhandos@gmail.com>
2023-02-15 19:57:53 +0600

    add static and media

--- website/urls.py
+++ website/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from crm.views import first_page, thanks_page
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', first_page, name='index'),
     path('thanks/', thanks_page, name='thanks'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
