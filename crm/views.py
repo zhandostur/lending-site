c683b8596f15b9c895ea7a72d2cc300da84245a5
zhandos <tursynbaevzhandos@gmail.com>
2023-02-16 21:21:58 +0600

    add app cms

--- crm/views.py
+++ crm/views.py
@@ -2,12 +2,21 @@ from django.shortcuts import render
 from .models import Order
 from .forms import OrderForm
 from cms.models import CmsSlider
+from price.models import PriceCard, PriceTable
 
 
 def first_page(request):
     slider_list = CmsSlider.objects.all()
+    pc_1 = PriceCard.objects.get(pk=1)
+    pc_2 = PriceCard.objects.get(pk=2)
+    pc_3 = PriceCard.objects.get(pk=3)
+    price_table = PriceTable.objects.all()
     context = {
         'slider_list': slider_list,
+        'pc_1': pc_1,
+        'pc_2': pc_2,
+        'pc_3': pc_3,
+        'price_table': price_table,
     }
     return render(request, 'index.html', context)
 
--- website/settings.py
+++ website/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'crm',
     'cms',
+    'price',
 ]
 
 MIDDLEWARE = [
