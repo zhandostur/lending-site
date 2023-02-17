fe027046b83f43cf5d7f32e4c787fa6850f07d78
zhandos <tursynbaevzhandos@gmail.com>
2023-02-17 15:13:09 +0600

    Обработка админ панеля

--- crm/admin.py
+++ crm/admin.py
@@ -1,6 +1,28 @@
 from django.contrib import admin
 from .models import Order, StatusCrm, ComentCrm
 
-admin.site.register(Order)
+
+class Coment(admin.StackedInline):
+    model = ComentCrm
+    fields = ['coment_dt', 'coment_text']
+    readonly_fields = ['coment_dt']
+    extra = 0
+
+
+class OrderAdm(admin.ModelAdmin):
+    list_display = ['id', 'order_status', 'order_name', 'order_phone', 'order_dt']
+    list_display_links = ['id', 'order_name']
+    search_fields = ['name', 'id', 'order_phone']
+    list_filter = ['order_status']
+    list_editable = ['order_status']
+    list_per_page = 10
+    list_max_show_all = 100
+    fields = ['id', 'order_status', 'order_dt', 'order_name', 'order_phone']
+    readonly_fields = ['order_dt', 'id']
+
+    inlines = [Coment]
+
+
+admin.site.register(Order, OrderAdm)
 admin.site.register(StatusCrm)
 admin.site.register(ComentCrm)
